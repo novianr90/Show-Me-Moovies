8ed854c59889767c2b8164d59350b54f55b0592d
Novian Rachman Iskandar <novian19@upi.edu>
2023-10-21 00:03:55 +0700

    refactor: add details and fav on feature module

---
+++ feature/favorites/build.gradle.kts
@@ -0,0 +1,54 @@
+import DependenciesImplementation.coroutines
+import DependenciesImplementation.glide
+import DependenciesImplementation.hilt
+import DependenciesImplementation.layout
+import DependenciesImplementation.viewModel
+
+plugins {
+    id(ProjectPlugin.androidLib)
+    id(ProjectPlugin.kotlinAndroid)
+    kotlin(ProjectPlugin.kapt)
+    id(ProjectPlugin.hilt)
+}
+
+android {
+    namespace = "id.novian.feature.favorites"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(project(":common:base"))
+    implementation(project(":common:domain"))
+
+    hilt()
+    layout()
+    viewModel()
+    glide()
+    coroutines()
+}
