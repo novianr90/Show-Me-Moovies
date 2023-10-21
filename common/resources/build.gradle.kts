de263899e9864f450c5741948c4d4e417f1a9311
Novian Rachman Iskandar <novian19@upi.edu>
2023-10-21 10:11:22 +0700

    refactor: add resources on common module

---
+++ common/resources/build.gradle.kts
@@ -0,0 +1,40 @@
+import DependenciesImplementation.core
+
+plugins {
+    id(ProjectPlugin.androidLib)
+    id(ProjectPlugin.kotlinAndroid)
+}
+
+android {
+    namespace = "id.novian.common.resources"
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+
+        testInstrumentationRunner = AppConfig.testInstrumentationRunner
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    core()
+}
